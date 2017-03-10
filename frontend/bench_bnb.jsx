79fd32ccecc79c5827ec42bea7c986b5ebdb410b
Dylan <dylan.patrick.cheng@gmail.com>
2017-03-10 12:38:23 -0800

    finish Phase1: Front-end auth

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './util/session_api_util.js';
+import { signup, login, logout } from './actions/session_actions';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>BenchBnB!</h1>, root);
+
+  let preloadedState;
+  if (window.currentUser) {
+    preloadedState = { session: { currentUser: window.currentUser } };
+  } else {
+    preloadedState = {};
+  }
+
+  const store = configureStore(preloadedState);
+
+  window.store = store;
   window.signup = signup;
   window.logout = logout;
   window.login = login;
